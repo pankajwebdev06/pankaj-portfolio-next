dff40ee99523312809516cb643cafcbf8a565ccb
Pankaj Kumar <pankajwebdev06@gmail.com>
2023-04-09 14:45:03 +0530

    Pankaj Portfolio by nextjs

---
+++ src/components/Skills.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import { motion } from 'framer-motion';
+
+const Skill = ({ name, x, y }) => {
+    return (
+        <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light lg:py-2 lg:lx-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent xs:text-dark xs:dark:text-light xs:text-bold'
+            whileHover={{ scale: 1.05 }}
+            initial={{ x: 0, y: 0 }}
+            whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
+            viewport={{ once: true }}
+        >
+            {name}
+        </motion.div>
+    )
+}
+
+const Skills = () => {
+    return (
+        <>
+            <h2 className='font-bold text-7xl mt-64 w-full text-center md:text-5xl md:mt-32'>
+                Skills
+            </h2>
+            <div className='w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] lg:bg-circularLightLg lg:dark:bg-circularDarkLg
+            md:bg-circularLightMd md:dark:bg-circularDarkMd
+            sm:bg-circularLightSm sm:dark:bg-circularDarkSm
+            '>
+                <motion.div className='flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer dark:text-dark dark:bg-light lg:p-6 md:p-4 xs:text-xs xs:p-2'
+                    whileHover={{ scale: 1.05 }}
+                >
+                    Web
+                </motion.div>
+
+                <Skill name="HTML" x="-21vw" y="-3vw" />
+                <Skill name="CSS" x="-5vw" y="-8vw" />
+                <Skill name="JavaScript" x="20vw" y="4vw" />
+                <Skill name="ReactJs" x="0vw" y="12vw" />
+                <Skill name="NextJs" x="-24vw" y="-15vw" />
+                <Skill name="NodeJs" x="19vw" y="-12vw" />
+                <Skill name="Express" x="35vw" y="-5vw" />
+                <Skill name="MongoDB" x="0vw" y="-20vw" />
+                <Skill name="TailwindCSS" x="-24vw" y="15vw" />
+                <Skill name="Bootstrap" x="18vw" y="17vw" />
+            </div>
+        </>
+    )
+}
+
+export default Skills
